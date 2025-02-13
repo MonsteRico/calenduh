4af5e185bac6a918f21a49ab211a00bab57ef9da
kellenneff <neff39@purdue.edu>
2025-02-12 20:49:58 -0500

    Created basic calendar list format

--- app/(app)/calendarsList.tsx
+++ app/(app)/calendarsList.tsx
@@ -1,17 +1,33 @@
 import { Button } from "@/components/Button";
 import { router } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import { Checkbox } from "@/components/Checkbox";
 
 export default function CalendarsList() {
       const isPresented = router.canGoBack();
+
     return (
         <View>
-            {isPresented && <Button onPress={() => {
-                router.back();
-            }} labelClasses="text-primary">
-                Go Back
-            </Button>}
-            <Text className="text-primary">Modal screen</Text>
+            <View className='bg-border items-left flex-row items-center justify-between'>
+                <View className="flex-row items-center m-2">
+                {isPresented && <Button onPress={() => {
+                    router.back();
+                }} labelClasses="text-secondary">
+                    Back
+                </Button>}
+                <Text className="text-2xl font-bold pl-5">Calendars</Text>
+                </View>
+                <View className="mr-3">
+                    <Button labelClasses="text-secondary">Add Calendar</Button>
+                </View>
+
+            </View>
+
+            <View className="m-3">
+                <Checkbox label="Calendar1" labelClasses="text-xl" color='#0000FF' checkSymbol={false}/>
+                <Checkbox label="Calendar2" labelClasses="text-xl"/>
+                       
+            </View>
         </View>
     );
 }
