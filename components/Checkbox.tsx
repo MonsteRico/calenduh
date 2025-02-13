80728ae8f787bdadd44c275cb9b8e9fa9f79335b
kellenneff <neff39@purdue.edu>
2025-02-12 20:50:10 -0500

    Added color and option to hide check symbol

--- components/Checkbox.tsx
+++ components/Checkbox.tsx
@@ -8,12 +8,16 @@ interface CheckboxProps extends React.ComponentPropsWithoutRef<typeof View> {
   label?: string;
   labelClasses?: string;
   checkboxClasses?: string;
+  color?: string; // border and fill color of box in hex
+  checkSymbol?: boolean; // enable/disable check symbol in box
 }
 function Checkbox({
   label,
   labelClasses,
   checkboxClasses,
   className,
+  color = '#525f7f', //gray-700 in hex
+  checkSymbol = true,
   ...props
 }: CheckboxProps) {
   const [isChecked, setChecked] = useState(false);
@@ -30,14 +34,18 @@ function Checkbox({
       <TouchableOpacity onPress={toggleCheckbox}>
         <View
           className={cn(
-            'w-4 h-4 border border-gray-700 rounded bg-background flex justify-center items-center',
+            'w-4 h-4 border rounded bg-background flex justify-center items-center',
             {
               'bg-foreground': isChecked,
             },
             checkboxClasses
           )}
+          style={{
+            borderColor: color,
+            backgroundColor: isChecked ? color: 'transparent'
+          }}
         >
-          {isChecked && <Text className="text-background text-xs">✓</Text>}
+          {isChecked && checkSymbol && <Text className="text-background text-xs">✓</Text>}
         </View>
       </TouchableOpacity>
       {label && (
