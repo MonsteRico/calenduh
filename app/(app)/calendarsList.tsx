282c94f15ac6838ed51b78ffbc13038a008cc6f4
kellenneff <neff39@purdue.edu>
2025-02-16 12:36:51 -0500

    Updated calendarList for calendar viewing

--- app/(app)/calendarsList.tsx
+++ app/(app)/calendarsList.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/Button";
 import { router } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Checkbox } from "@/components/Checkbox";
 import { Accordion } from "@/components/Accordion";
+import { useState } from "react";
+import { CalendarItem } from "@/components/CalendarItem";
+import { CalendarModal } from "@/components/CalendarModal";
 
 interface example_calendar {
     name: string;
@@ -12,57 +15,59 @@ interface example_calendar {
 const calendars: example_calendar[] = [
     { name: "Calendar1", color: "#0000FF" },
     { name: "Calendar2", color: "#d42245" },
-    { name: "Calendar3", color: "#0a571e"}
+    { name: "Calendar3", color: "#0a571e" }
 ];
 
 
 export default function CalendarsList() {
-      const isPresented = router.canGoBack();
+    const isPresented = router.canGoBack();
+    const [modalVisible, setModalVisible] = useState(false);
 
     return (
+
         <View>
+
+            <CalendarModal visible={modalVisible} name="Test" color="test" onClose={() => setModalVisible(!modalVisible)} />
             <View className='bg-border items-left flex-row items-center justify-between'>
                 <View className="flex-row items-center m-2">
-                {isPresented && <Button onPress={() => {
-                    router.back();
-                }} labelClasses="text-secondary">
-                    Back
-                </Button>}
-                <Text className="text-3xl font-bold pl-5">Calendars</Text>
+                    {isPresented && <Button onPress={() => {
+                        router.back();
+                    }} labelClasses="text-secondary">
+                        Back
+                    </Button>}
+                    <Text className="text-3xl font-bold pl-5">Calendars</Text>
                 </View>
-                <View className="mr-3">
-                    <Button labelClasses="text-secondary">Add Calendar</Button>
-                </View>
+                <Button labelClasses="text-secondary" className="mr-5">Add Calendar</Button>
 
             </View>
             <Accordion title={"Owned Calendars"} className="mb-5">
-            <View className="m-5">
-                {calendars.map((calendar) => (
-                    <View className="mb-3">
-                        <Checkbox key={calendar.name} label={calendar.name} color={calendar.color} labelClasses="text-2xl" checkSymbol={false}/>
-                    </View>
-                ))}
-            </View>
+                <View className="m-5">
+                    {calendars.map((calendar) => (
+                        <View className="mb-3">
+                            <CalendarItem key={calendar.name} name={calendar.name} color={calendar.color} onPress={() => { router.navigate("/calendarInfoView") }} />
+                        </View>
+                    ))}
+                </View>
             </Accordion>
 
             <Accordion title={"Group Calendars"} className="mb-5">
-            <View className="m-5">
-                {calendars.map((calendar) => (
-                    <View className="mb-3">
-                        <Checkbox key={calendar.name} label={calendar.name} color={calendar.color} labelClasses="text-2xl" checkSymbol={false}/>
-                    </View>
-                ))}
-            </View>
+                <View className="m-5">
+                    {calendars.map((calendar) => (
+                        <View className="mb-3">
+                            <CalendarItem key={calendar.name} name={calendar.name} color={calendar.color} />
+                        </View>
+                    ))}
+                </View>
             </Accordion>
-            
+
             <Accordion title={"Other Calendars"}>
-            <View className="m-5">
-                {calendars.map((calendar) => (
-                    <View className="mb-3">
-                        <Checkbox key={calendar.name} label={calendar.name} color={calendar.color} labelClasses="text-2xl" checkSymbol={false}/>
-                    </View>
-                ))}
-            </View>
+                <View className="m-5">
+                    {calendars.map((calendar) => (
+                        <View className="mb-3">
+                            <CalendarItem key={calendar.name} name={calendar.name} color={calendar.color} />
+                        </View>
+                    ))}
+                </View>
             </Accordion>
         </View>
     );
