d00ed6ad3bc54812ad8c40be9113be1b58a70d7f
EddieN03 <eddiefalcon20@gmail.com>
2025-03-30 22:42:35 -0400

    Group Hooks
    Followed along with what we had for cals, commented out the local ones because I think we agreed that local groups will NOT exist

--- types/mutation.types.ts
+++ types/mutation.types.ts
@@ -5,6 +5,7 @@ export interface Mutation {
     parameters: string;
     calendar_id?: string;
 	event_id?: string;
+	group_id?: string; //Maybe?
 }
 
 export type MutationTypes =
@@ -13,4 +14,7 @@ export type MutationTypes =
 	| "DELETE_CALENDAR"
 	| "CREATE_EVENT"
 	| "UPDATE_EVENT"
-	| "DELETE_EVENT";
+	| "DELETE_EVENT"
+	| "CREATE_GROUP" //Again idk if this is needed
+	| "UPDATE_GROUP"
+	| "DELETE_GROUP";
