3bff5c3d3819c842cc3e364576feae9930c67fc9
EddieN03 <eddiefalcon20@gmail.com>
2025-04-16 16:05:57 -0400

    Locks Public Cals to Real Accs
    I don't like local accounts

--- app/(app)/browsePublicCalendars.tsx
+++ app/(app)/browsePublicCalendars.tsx
@@ -11,6 +11,15 @@ import { getCalendarsFromDB } from "@/lib/calendar.helpers";
 import { useIsConnected } from "@/hooks/useIsConnected";
 import { useEnabledCalendarIds } from "@/hooks/useEnabledCalendarIds";
 import { setEnabled } from "react-native/Libraries/Performance/Systrace";
+import { DismissKeyboardView } from "@/components/DismissKeyboardView";
+import { JoinGroupModal } from "@/components/JoinGroupModal";
+import { CreateGroupModal } from '@/components/CreateGroupModal';
+import { ViewGroupModal } from "@/components/ViewGroupModal";
+import { Group } from '@/types/group.types';
+import { EditGroupModal } from '@/components/EditGroupModal';
+import { useMyGroups } from "@/hooks/group.hooks";
+import { Feather } from '@expo/vector-icons';
+import { useSession } from "@/hooks/authContext";
 
 export default function BrowsePublicCalendars() {
 	const isPresented = router.canGoBack();
@@ -20,5 +29,29 @@ export default function BrowsePublicCalendars() {
 	const db = useSQLiteContext();
 
 	const { setEnabledCalendarIds } = useEnabledCalendarIds();
-	return <View className="flex-1 bg-background"></View>;
+
+	const { user } = useSession();
+
+	if (user?.user_id === "localUser") {
+		return (
+			<View>
+				<View className="px-4 pt-6 bg-primary/5">
+					<Text className="text-3xl font-bold text-primary mb-6">Public Calendars</Text>
+				</View>
+
+
+				<View className="items-center justify-center py-12">
+					<View className="bg-gray-100 rounded-full p-4 mb-4">
+						<Feather name="users" size={24} color="#6366f1" />
+					</View>
+					<Text className="text-lg font-semibold text-gray-800 text-center">Subcribing is not Enabled for Guest Accounts</Text>
+					<Text className="text-gray-500 text-center mt-2 mb-6">Create an account to subscribe to calendars</Text>
+				</View>
+			</View>
+		)
+	}
+
+	return <View className="flex-1 bg-background">
+
+	</View>;
 }
